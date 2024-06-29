960cb737212dcefc41b5af8446ead47b59342e7a
padhmavignesh <rpadhmavignesh@gmail.com>
2024-06-29 13:55:35 +0530

    End of Section 4

--- WalksNZ/WalksNZAPI/Controllers/RegionsController.cs
+++ WalksNZ/WalksNZAPI/Controllers/RegionsController.cs
@@ -26,5 +26,102 @@ namespace WalksNZAPI.Controllers
             var regionDto = mapper.Map<List<RegionDTO>>(regions);
             return Ok(regionDto);
         }
+        [HttpGet]
+        [Route("{id:Guid}")]
+        [ActionName("GetRegionAsync")]
+        public async Task<IActionResult> GetRegionAsync([FromRoute]Guid id)
+        {
+            var regions = await regionRepository.GetAsync(id);
+            if(regions == null)
+            {
+                return NotFound();
+            }
+            var regionDto = mapper.Map<RegionDTO>(regions);
+            return Ok(regionDto);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddRegionAsync(AddRegionRequest item)
+        {
+            var region = new Region()
+            {
+                Name = item.Name,
+                Code = item.Code,
+                Area = item.Area,
+                Lat = item.Lat,
+                Long = item.Long,
+                Population = item.Population
+            };
+
+            region = await regionRepository.AddAsync(region);
+
+            var regionDto = new RegionDTO()
+            {
+                Id = region.Id,
+                Name = region.Name,
+                Code = region.Code,
+                Area = region.Area,
+                Lat = region.Lat,
+                Long = region.Long,
+                Population = region.Population
+            };
+            return CreatedAtAction(nameof(GetRegionAsync), new { id = regionDto.Id },regionDto);
+        }
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteRegionAsync(Guid id)
+        {
+            var region = await regionRepository.DeleteAsync(id);
+
+            if(region == null) 
+            { 
+                return NotFound();
+            }
+
+            var regionDto = new RegionDTO()
+            {
+                Id = region.Id,
+                Name = region.Name,
+                Code = region.Code,
+                Area = region.Area,
+                Lat = region.Lat,
+                Long = region.Long,
+                Population = region.Population
+            };
+
+            return Ok(regionDto);
+        }
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateRegionAsync([FromRoute] Guid id,[FromBody] UpdateRegionRequest item)
+        {
+            var region = new Region()
+            {
+                Name = item.Name,
+                Code = item.Code,
+                Area = item.Area,
+                Lat = item.Lat,
+                Long = item.Long,
+                Population = item.Population
+            };
+
+            region = await regionRepository.UpdateAsync(id, region);
+            if(region == null)
+            {
+                return NotFound();
+            }
+
+            var regionDto = new RegionDTO()
+            {
+                Id = region.Id,
+                Name = region.Name,
+                Code = region.Code,
+                Area = region.Area,
+                Lat = region.Lat,
+                Long = region.Long,
+                Population = region.Population
+            };
+
+            return Ok(regionDto);
+        }
     }
 }
--- WalksNZ/WalksNZAPI/Repositories/RegionRepository.cs
+++ WalksNZ/WalksNZAPI/Repositories/RegionRepository.cs
@@ -12,9 +12,54 @@ namespace WalksNZAPI.Repositories
             this._context = _context;
         }
 
+        public async Task<Region> AddAsync(Region region)
+        {
+            region.Id = Guid.NewGuid();
+            await _context.AddAsync(region);
+            await _context.SaveChangesAsync();
+            return region;
+        }
+
+        public async Task<Region> DeleteAsync(Guid id)
+        {
+            var region = await _context.Regions.FirstOrDefaultAsync(x => x.Id == id);
+            if (region == null)
+            {
+                return null;
+            }
+            _context.Regions.Remove(region);
+            await _context.SaveChangesAsync();
+            return region;
+        }
+
         public async Task<IEnumerable<Region>> GetAllAsync()
         {
             return await _context.Regions.ToListAsync();
+        }
+
+        public async Task<Region> GetAsync(Guid id)
+        {
+            return await _context.Regions.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Region> UpdateAsync(Guid id, Region region)
+        {
+            var existingRegion = await _context.Regions.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (existingRegion == null)
+            {
+                return null;
+            }
+
+            existingRegion.Name = region.Name;
+            existingRegion.Code = region.Code;
+            existingRegion.Area = region.Area;
+            existingRegion.Lat = region.Lat;
+            existingRegion.Long = region.Long;
+            existingRegion.Population = region.Population;
+
+            await _context.SaveChangesAsync();
+            return existingRegion;
         }
     }
 }
